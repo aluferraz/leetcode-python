fa3e23eb72e0261cdfbae26ba82c19e53b953351
Lucas Mohallem Ferraz <ferraz.m.lucas@gmail.com>
2023-10-14 11:13:22 -0300

    131023

--- leetcode/editor/en/Q505/TheMazeIi.py
+++ leetcode/editor/en/Q505/TheMazeIi.py
@@ -1,3 +1,4 @@
+import collections
 from typing import List
 
 
@@ -22,41 +23,46 @@ class Solution(object):
         def isValidIdx(i, j):
             return i >= 0 and i < N and j >= 0 and j < M and maze[i][j] == 0
 
-        explored = {}
-        exploring = set()
+        if not isValidIdx(start[0], start[1]):
+            if start == destination:
+                return 0
         INF = 10 ** 20
+
+        costs = [[[INF for _ in range(len(DIRECTIONS))] for _ in range(M)] for _ in range(N)]
+
+        q = collections.deque()
+        q.append((start[0], start[1], 0))
+        q.append((start[0], start[1], 1))
+        q.append((start[0], start[1], 2))
+        q.append((start[0], start[1], 3))
+        costs[start[0]][start[1]][0] = 0
+        costs[start[0]][start[1]][1] = 0
+        costs[start[0]][start[1]][2] = 0
+        costs[start[0]][start[1]][3] = 0
         ans = INF
-
-        def explore(i, j, dir):
-
-            if (i, j, dir) in explored:
-                return explored[(i, j, dir)]
-            best = INF
-
-            exploring.add((i, j, dir))
-            new_i, new_j = (i + DIRECTIONS[dir][0]), (j + DIRECTIONS[dir][1])
-            if isValidIdx(new_i, new_j):
-                best = min(best, 1 + explore(new_i, new_j, dir))
-            else:
-                # Stoped
-                if [i, j] == destination:
-                    exploring.discard((i, j, dir))
-                    explored[(i, j, dir)] = 0
-                    return 0
-                for new_dir in range(len(DIRECTIONS)):
-                    if new_dir == dir:
-                        continue
-                    new_i, new_j = (i + DIRECTIONS[new_dir][0]), (j + DIRECTIONS[new_dir][1])
-                    if (new_i, new_j, new_dir) in exploring:
-                        continue
-                    if isValidIdx(new_i, new_j):
-                        best = min(best, 1 + explore(new_i, new_j, new_dir))
-            exploring.discard((i, j, dir))
-            explored[(i, j, dir)] = best
-            return best
-
-        for i in range(len(DIRECTIONS)):
-            ans = min(ans, explore(start[0], start[1], i))
+        while len(q) > 0:
+            size = len(q)
+            for _ in range(size):
+                y, x, d = q.popleft()
+                dy, dx = DIRECTIONS[d]
+                cost_here = costs[y][x][d]
+                my, mx = y + dy, x + dx
+                if isValidIdx(my, mx):
+                    if cost_here + 1 < costs[my][mx][d]:
+                        costs[my][mx][d] = cost_here + 1
+                        q.append((my, mx, d))
+                else:
+                    # Stopped
+                    if [y, x] == destination:
+                        ans = min(ans, cost_here)
+                    for i in range(len(DIRECTIONS)):
+                        d = i
+                        dy, dx = DIRECTIONS[i]
+                        my, mx = y + dy, x + dx
+                        if isValidIdx(my, mx):
+                            if cost_here + 1 < costs[my][mx][d]:
+                                costs[my][mx][d] = cost_here + 1
+                                q.append((my, mx, d))
         if ans >= INF:
             return -1
         return ans

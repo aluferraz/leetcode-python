d0681ce4c257397893788c39c206383ab79dc35f
Lucas Mohallem Ferraz <ferraz.m.lucas@gmail.com>
2023-10-17 04:03:05 -0300

    161023

--- leetcode/editor/en/Q119/PascalsTriangleIi.py
+++ leetcode/editor/en/Q119/PascalsTriangleIi.py
@@ -1,5 +1,6 @@
 from typing import List
 
+
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution(object):
     def getRow(self, rowIndex):
@@ -7,10 +8,22 @@ class Solution(object):
         :type rowIndex: int
         :rtype: List[int]
         """
-        
+
+        def pascals(row):
+            if row == 0:
+                return [1]
+            N = row + 1
+            prev = pascals(row - 1)
+            current = [1] * N
+            for i in range(1, N - 1):
+                current[i] = prev[i] + prev[i - 1]
+            return current
+
+        return pascals(rowIndex)
+
+
 # leetcode submit region end(Prohibit modification and deletion)
 
 
 class PascalsTriangleIi(Solution):
     pass
-    
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from contest import Solution
+from leetcode.editor.en.Q119.PascalsTriangleIi import PascalsTriangleIi
 from leetcode.editor.en.Q2009.MinimumNumberOfOperationsToMakeArrayContinuous import \
     MinimumNumberOfOperationsToMakeArrayContinuous
 from leetcode.editor.en.Q2251.NumberOfFlowersInFullBloom import NumberOfFlowersInFullBloom
@@ -8,4 +9,4 @@ from leetcode.editor.en.Q505.TheMazeIi import TheMazeIi
 dummy = Solution()
 
 if __name__ == '__main__':
-    print(PaintingTheWalls().paintWalls(cost=[1, 2, 3, 2], time=[1, 2, 3, 2]))
+    print(PascalsTriangleIi().getRow(3))

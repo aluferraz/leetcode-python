af47cc29997297d03f2a610422716083e636a8f1
Lucas Mohallem Ferraz <ferraz.m.lucas@gmail.com>
2023-11-04 06:11:34 -0300

    031123

--- leetcode/editor/en/Q1441/BuildAnArrayWithStackOperations.py
+++ leetcode/editor/en/Q1441/BuildAnArrayWithStackOperations.py
@@ -1,5 +1,6 @@
 from typing import List
 
+
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution(object):
     def buildArray(self, target, n):
@@ -8,10 +9,25 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
-        
-# leetcode submit region end(Prohibit modification and deletion)
+        stack = []
+        target.reverse()
+        ans = []
+        for i in range(1, n + 1):
+            while len(stack) > 0 and stack[-1] < target[-1]:
+                ans.append("Pop")
+                stack.pop()
+
+            ans.append("Push")
+            stack.append(i)
+            if stack[-1] == target[-1]:
+                target.pop()
+                stack.append(10 ** 20)  # stop marker
+            if len(target) == 0:
+                break
+
+        return ans
+    # leetcode submit region end(Prohibit modification and deletion)
 
 
 class BuildAnArrayWithStackOperations(Solution):
     pass
-    
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from contest import Solution
 from leetcode.editor.en.Q1220.CountVowelsPermutation import CountVowelsPermutation
+from leetcode.editor.en.Q1441.BuildAnArrayWithStackOperations import BuildAnArrayWithStackOperations
 from leetcode.editor.en.Q2906.ConstructProductMatrix import ConstructProductMatrix
 from leetcode.editor.en.Q458.PoorPigs import PoorPigs
 
 dummy = Solution()
 
 if __name__ == '__main__':
-    print(PoorPigs().poorPigs(buckets=8, minutesToDie=15, minutesToTest=15))
+    print(BuildAnArrayWithStackOperations().buildArray(target = [1,2], n = 4))

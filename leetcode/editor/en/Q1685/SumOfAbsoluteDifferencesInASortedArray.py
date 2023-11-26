480d12b390d712d7a9759f6c42ca7a8b9532efb0
Lucas Mohallem Ferraz <ferraz.m.lucas@gmail.com>
2023-11-26 04:51:13 -0300

    251123

--- leetcode/editor/en/Q1685/SumOfAbsoluteDifferencesInASortedArray.py
+++ leetcode/editor/en/Q1685/SumOfAbsoluteDifferencesInASortedArray.py
@@ -1,5 +1,6 @@
 from typing import List
 
+
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution(object):
     def getSumAbsoluteDifferences(self, nums):
@@ -7,10 +8,29 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        
-# leetcode submit region end(Prohibit modification and deletion)
+
+        abs_diff = 0
+        N = len(nums)
+        presum = [0] * N
+        presum[0] = nums[0]
+        for i in range(1, N):
+            presum[i] = presum[i - 1] + nums[i]
+
+        ans = [-1] * N
+        ahead = 0
+        for i in range(N - 1, -1, -1):
+            if i + 1 < N:
+                elements_ahead = (N - 1 - i)
+                diff = abs(nums[i] - ahead)
+                abs_diff += (diff * (elements_ahead - 1)) + diff
+            ans_here = abs(((i + 1) * nums[i]) - (presum[i])) + abs_diff
+            ans[i] = ans_here
+            ahead = nums[i]
+
+        return ans
+
+        # leetcode submit region end(Prohibit modification and deletion)
 
 
 class SumOfAbsoluteDifferencesInASortedArray(Solution):
     pass
-    
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from contest import Solution
 from leetcode.editor.en.Q1561.MaximumNumberOfCoinsYouCanGet import MaximumNumberOfCoinsYouCanGet
+from leetcode.editor.en.Q1685.SumOfAbsoluteDifferencesInASortedArray import SumOfAbsoluteDifferencesInASortedArray
 from leetcode.editor.en.Q624.MaximumDistanceInArrays import MaximumDistanceInArrays
 
 dummy = Solution()
 
 if __name__ == '__main__':
-    print(MaximumDistanceInArrays().maxDistance([[-1, 1], [-3, 1, 4], [-2, -1, 0, 2]]))
+    print(SumOfAbsoluteDifferencesInASortedArray().getSumAbsoluteDifferences([1,4,6,8,10]))

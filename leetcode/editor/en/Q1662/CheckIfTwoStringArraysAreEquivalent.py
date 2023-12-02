25572874be3146db8009969679f8a42c81b05ccc
Lucas Mohallem Ferraz <ferraz.m.lucas@gmail.com>
2023-12-02 07:44:29 -0300

    011223

---
+++ leetcode/editor/en/Q1662/CheckIfTwoStringArraysAreEquivalent.py
@@ -0,0 +1,12 @@
+from typing import List
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+        return "".join(word1) == "".join(word2)
+# leetcode submit region end(Prohibit modification and deletion)
+
+
+class CheckIfTwoStringArraysAreEquivalent(Solution):
+    pass
+    

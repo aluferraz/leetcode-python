12ce3c1ab7ec52439df03b8d49b1bf1d93144476
Lucas Mohallem Ferraz <ferraz.m.lucas@gmail.com>
2024-01-15 04:22:50 -0600

    140124

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from contest import Solution
 from leetcode.editor.en.Q1347.MinimumNumberOfStepsToMakeTwoStringsAnagram import \
     MinimumNumberOfStepsToMakeTwoStringsAnagram
+from leetcode.editor.en.Q1657.DetermineIfTwoStringsAreClose import DetermineIfTwoStringsAreClose
 from leetcode.editor.en.Q300.LongestIncreasingSubsequence import LongestIncreasingSubsequence
 from leetcode.editor.en.Q446.ArithmeticSlicesIiSubsequence import ArithmeticSlicesIiSubsequence
 
 dummy = Solution()
 
 if __name__ == '__main__':
-    print(MinimumNumberOfStepsToMakeTwoStringsAnagram().minSteps("ftngfmcxlfxitnoanvnnfdswndjhgclcbcyrkcwqwlglqylgrcbr", t = "mnpyqiuogwvgjsrzljxryzsqlxatmpkauuemgkxmgshkfdwnpcet"))
+    print(DetermineIfTwoStringsAreClose().closeStrings("cabbba","abbccc"))

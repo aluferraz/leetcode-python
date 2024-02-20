ec20727cb4db90c679622136b46c8460893f253a
Lucas Mohallem Ferraz <ferraz.m.lucas@gmail.com>
2024-02-20 05:48:09 -0600

    190224

--- leetcode/editor/en/Q2402/MeetingRoomsIii.py
+++ leetcode/editor/en/Q2402/MeetingRoomsIii.py
@@ -1,40 +1,50 @@
 import collections
 import heapq
-from typing import List, Optional
+from typing import List
+
 
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
-        rooms = [(-1,i) for i in range(n)]
         START = 1
         END = -1
         timeline = []
         counter = collections.defaultdict(int)
-        delays = collections.defaultdict(int)
         meetings_map = {}
+        meetings_prio = []
         for i in range(len(meetings)):
-            s,e = meetings[i]
-            timeline.append((START, s,i))
-            timeline.append((END, e, i))
-            meetings_map[i] = (s,e)
-        heapq.heapify(timeline)
-        heapq.heapify(rooms)
+            s, e = meetings[i]
+            meetings_map[i] = [s, e, -1]
+            meetings_prio.append((s, e, i))
+        heapq.heapify(meetings_prio)
+
+        free_rooms = [i for i in range(n)]
+        for rid in range(n):
+            s, e, i = heapq.heappop(meetings_prio)
+            heapq.heappush(timeline, (s, START, rid, i))
+            if len(meetings_prio) == 0:
+                break
 
         while len(timeline) > 0:
-            evt, time, id = heapq.heappop(timeline)
+            time, evt, rid, id = heapq.heappop(timeline)
             if evt == END:
-                if id in delays:
-                    timeline.append((END, time + delays[id], id))
+                heapq.heappush(free_rooms, meetings_map[id][2])
+                if len(meetings_prio) > 0:
+                    s, e, i = heapq.heappop(meetings_prio)
+                    if time > s:
+                        delay = time - s
+                        duration = e - s
+                        s += delay
+                        e = s + duration
+                    meetings_map[i][0] = s
+                    meetings_map[i][1] = e
+                    heapq.heappush(timeline, (s, START, rid, i))
             else:
-                if rooms[0][0] > time:
-                    delay = rooms[0][0] - time
-                    timeline.append((START, time + delay, id))
-                    delays[id] = delay
-
-                rtime, rid = heapq.heappop(rooms)
+                rid = heapq.heappop(free_rooms)
                 counter[rid] += 1
                 rtime = meetings_map[id][1]
-                heapq.heappush(rooms, (rtime, rid))
+                meetings_map[id][2] = rid
+                heapq.heappush(timeline, (rtime, END, rid, id))
 
         ans = 0
         ans_room = 0
@@ -43,7 +53,6 @@ class Solution:
                 ans = counter[i]
                 ans_room = i
         return ans_room
-
 
 
 # leetcode submit region end(Prohibit modification and deletion)

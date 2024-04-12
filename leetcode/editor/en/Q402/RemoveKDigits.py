609726797763143fac73590be3b0a5ffd279b8cc
Lucas Mohallem Ferraz <ferraz.m.lucas@gmail.com>
2024-04-12 04:49:05 -0600

    110424

--- leetcode/editor/en/Q402/RemoveKDigits.py
+++ leetcode/editor/en/Q402/RemoveKDigits.py
@@ -5,13 +5,23 @@ class Solution:
     def removeKdigits(self, num: str, k: int) -> str:
         ans = []
         N = len(num)
+        if k >= N:
+            return "0"
         final_len = N - k
         for i in range(N):
             digit = int(num[i])
-            while len(ans) > 0 and digit < ans[-1] and (N - i + len(ans) ) >= final_len:
+            while len(ans) > 0 and digit < ans[-1] and ((N - (i + 1)) + len(ans)) >= final_len:
                 ans.pop()
             ans.append(digit)
-        return "".join(ans)
+        while len(ans) > final_len:
+            ans.pop()
+
+        ans_s = "".join(str(d) for d in ans)
+        left = 0
+        while left < (len(ans_s) - 1) and ans_s[left] == "0":
+            left += 1
+        return ans_s[left::]
+
 
 
 # leetcode submit region end(Prohibit modification and deletion)
--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 from contest import Solution
-from leetcode.editor.en.Q1249.MinimumRemoveToMakeValidParentheses import MinimumRemoveToMakeValidParentheses
-from leetcode.editor.en.Q1614.MaximumNestingDepthOfTheParentheses import MaximumNestingDepthOfTheParentheses
-from leetcode.editor.en.Q2073.TimeNeededToBuyTickets import TimeNeededToBuyTickets
-from leetcode.editor.en.Q255.VerifyPreorderSequenceInBinarySearchTree import VerifyPreorderSequenceInBinarySearchTree
-from leetcode.editor.en.Q691.StickersToSpellWord import StickersToSpellWord
-from leetcode.editor.en.Q79.WordSearch import WordSearch
-from leetcode.editor.en.Q950.RevealCardsInIncreasingOrder import RevealCardsInIncreasingOrder
+from leetcode.editor.en.Q402.RemoveKDigits import RemoveKDigits
 
 dummy = Solution()
 
 if __name__ == '__main__':
-    print(RevealCardsInIncreasingOrder().deckRevealedIncreasing([1,2,3,4]))
+    print(RemoveKDigits().removeKdigits("112",1))

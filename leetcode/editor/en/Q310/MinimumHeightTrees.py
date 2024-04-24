9c3f8d4fd2c50b23f479031b169ce2ae105de46a
Lucas Mohallem Ferraz <ferraz.m.lucas@gmail.com>
2024-04-24 04:47:13 -0600

    230424

--- leetcode/editor/en/Q310/MinimumHeightTrees.py
+++ leetcode/editor/en/Q310/MinimumHeightTrees.py
@@ -1,9 +1,41 @@
-from typing import List, Optional
+import collections
+from functools import cache
+from typing import List
+
 
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
-        
+        adj_list = collections.defaultdict(list)
+        for u, v in edges:
+            adj_list[u].append(v)
+            adj_list[v].append(u)
+
+        leaf_nodes = []
+        for k in adj_list.keys():
+            if len(adj_list[k]) == 1:
+                leaf_nodes.append(k)
+
+        @cache
+        def get_height(p, node):
+            height = 0
+            for child in adj_list[node]:
+                if child != p:
+                    height = max(height, 1 + get_height(node, child))
+            return height
+
+        ans = []
+        min_height = n
+        for i in range(n):
+            height_here = get_height(i, i)
+            if height_here < min_height:
+                ans = []
+            elif height_here > min_height:
+                continue
+            ans.append(i)
+        return ans
+
+
 # leetcode submit region end(Prohibit modification and deletion)
 
 
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from contest import Solution
-from leetcode.editor.en.Q752.OpenTheLock import OpenTheLock
+from leetcode.editor.en.Q310.MinimumHeightTrees import MinimumHeightTrees
 
 dummy = Solution()
 
 if __name__ == '__main__':
-    print(OpenTheLock().openLock(["8887", "8889", "8878", "8898", "8788", "8988", "7888", "9888"]
-                                 , "8888"))
+    print(MinimumHeightTrees().findMinHeightTrees(n=4, edges=[[1, 0], [1, 2], [1, 3]]))

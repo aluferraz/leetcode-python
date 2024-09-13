0f98a331d8f399dff435b485184ffc25c0d1e823
Lucas Mohallem Ferraz <ferraz.m.lucas@gmail.com>
2024-09-13 04:19:43 -0600

    120924

---
+++ leetcode/editor/en/Q1310/XorQueriesOfASubarray.py
@@ -0,0 +1,11 @@
+from typing import List, Optional
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
+        
+# leetcode submit region end(Prohibit modification and deletion)
+
+
+class XorQueriesOfASubarray(Solution):
+    pass
--- leetcode/editor/en/Q1684/CountTheNumberOfConsistentStrings.py
+++ leetcode/editor/en/Q1684/CountTheNumberOfConsistentStrings.py
@@ -1,9 +1,30 @@
-from typing import List, Optional
+from typing import List
+
 
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
-        
+        ans = 0
+
+        def to_arr(s):
+            arr = [0] * 26
+            for c in s:
+                arr[ord(c) - ord('a')] = 1
+            return arr
+
+        allowed = to_arr(allowed)
+        for w in words:
+            w_arr = to_arr(w)
+            valid = True
+            for i in range(26):
+                if w_arr[i] == 1 and allowed[i] == 0:
+                    valid = False
+                    break
+            if valid:
+                ans += 1
+        return ans
+
+
 # leetcode submit region end(Prohibit modification and deletion)
 
 

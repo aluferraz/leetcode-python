260240f82a296102936c244fb7e5a19c97fffbe7
Lucas Mohallem Ferraz <ferraz.m.lucas@gmail.com>
2024-10-01 04:36:59 -0600

    300924

---
+++ leetcode/editor/en/Q1381/DesignAStackWithIncrementOperation.py
@@ -0,0 +1,31 @@
+# leetcode submit region begin(Prohibit modification and deletion)
+class CustomStack:
+
+    def __init__(self, maxSize: int):
+        self.stack = []
+        self.max_size = maxSize
+
+    def push(self, x: int) -> None:
+        if len(self.stack) < self.max_size:
+            self.stack.append(x)
+
+    def pop(self) -> int:
+        if len(self.stack) == 0:
+            return -1
+        return self.stack.pop()
+
+    def increment(self, k: int, val: int) -> None:
+        for i in range(min(k, len(self.stack))):
+            self.stack[i] += val
+
+
+# Your CustomStack object will be instantiated and called as such:
+# obj = CustomStack(maxSize)
+# obj.push(x)
+# param_2 = obj.pop()
+# obj.increment(k,val)
+# leetcode submit region end(Prohibit modification and deletion)
+
+
+class DesignAStackWithIncrementOperation(Solution):
+    pass

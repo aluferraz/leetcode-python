5a892ddea9f9ed9004c9d23e704a45720ca91995
Lucas Mohallem Ferraz <ferraz.m.lucas@gmail.com>
2024-10-14 04:52:19 -0600

    131024

--- leetcode/editor/en/Q2406/DivideIntervalsIntoMinimumNumberOfGroups.py
+++ leetcode/editor/en/Q2406/DivideIntervalsIntoMinimumNumberOfGroups.py
@@ -1,10 +1,41 @@
-from typing import List, Optional
-from functools import cache
+from typing import List
+
+import sortedcontainers
+
 
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def minGroups(self, intervals: List[List[int]]) -> int:
-        
+        intervals.sort()
+        groups = sortedcontainers.SortedList(key=lambda x: x[-1])
+        groups.add([0])
+
+        def find(start):
+            left = 0
+            right = len(groups)
+            ans = len(groups)
+            while left < right:
+                mid = (left + right) // 2
+                if groups[mid][-1] >= start:
+                    right = mid
+                else:
+                    ans = mid
+                    left = mid + 1
+            return ans
+
+        for s, e in intervals:
+            idx = find(s)
+            if idx == len(groups):
+                groups.add([e])
+            else:
+                group = groups[idx]
+                group.append(e)
+                groups.pop(idx)
+                groups.add(group)
+
+        return len(groups)
+
+
 # leetcode submit region end(Prohibit modification and deletion)
 
 
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from leetcode.editor.en.Q1813.SentenceSimilarityIii import SentenceSimilarityIii
+from leetcode.editor.en.Q2406.DivideIntervalsIntoMinimumNumberOfGroups import DivideIntervalsIntoMinimumNumberOfGroups
 
 if __name__ == '__main__':
-    print(SentenceSimilarityIii().areSentencesSimilar(sentence1="A", sentence2="a A b A"))
+    print(DivideIntervalsIntoMinimumNumberOfGroups().minGroups([[5, 10], [6, 8], [1, 5], [2, 3], [1, 10]]))

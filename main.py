9bdfc84aa75a468ba5703fb7353a9ddd2ccce8c2
Lucas Mohallem Ferraz <ferraz.m.lucas@gmail.com>
2024-11-30 15:25:24 -0600

    291124

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from leetcode.editor.en.Q773.SlidingPuzzle import SlidingPuzzle
+from leetcode.editor.en.Q2290.MinimumObstacleRemovalToReachCorner import MinimumObstacleRemovalToReachCorner
 
 if __name__ == '__main__':
-    print(SlidingPuzzle().slidingPuzzle([[1, 2, 3], [4, 0, 5]]
-                                        ))
+    print(MinimumObstacleRemovalToReachCorner().minimumObstacles([[0, 1, 0, 0, 0], [0, 1, 0, 1, 0], [0, 0, 0, 1, 0]]
+                                                                 ))

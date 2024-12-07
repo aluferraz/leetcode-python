afec8730d84e52b2e18741132a03fc1d0da292d1
Lucas Mohallem Ferraz <ferraz.m.lucas@gmail.com>
2024-12-07 09:23:21 -0600

    798 days

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from leetcode.editor.en.Q2290.MinimumObstacleRemovalToReachCorner import MinimumObstacleRemovalToReachCorner
+from leetcode.editor.en.Q2097.ValidArrangementOfPairs import ValidArrangementOfPairs
 
 if __name__ == '__main__':
-    print(MinimumObstacleRemovalToReachCorner().minimumObstacles([[0, 1, 0, 0, 0], [0, 1, 0, 1, 0], [0, 0, 0, 1, 0]]
-                                                                 ))
+    print(ValidArrangementOfPairs().validArrangement([[5, 1], [4, 5], [11, 9], [9, 4]]))

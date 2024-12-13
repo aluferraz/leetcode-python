d825ddb9e142ca96757752af12f6b825f7ca98d0
Lucas Mohallem Ferraz <ferraz.m.lucas@gmail.com>
2024-12-13 08:28:25 -0800

    121224

--- leetcode/editor/en/Q2558/TakeGiftsFromTheRichestPile.py
+++ leetcode/editor/en/Q2558/TakeGiftsFromTheRichestPile.py
@@ -1,10 +1,19 @@
-from typing import List, Optional
-from functools import cache
+import heapq
+import math
+from typing import List
+
 
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def pickGifts(self, gifts: List[int], k: int) -> int:
-        
+        gifts = [-x for x in gifts]
+        heapq.heapify(gifts)
+        for _ in range(k):
+            max_n = abs(heapq.heappop(gifts))
+            heapq.heappush(gifts, -1 * (math.floor(math.sqrt(max_n))))
+        return sum(abs(x) for x in gifts)
+
+
 # leetcode submit region end(Prohibit modification and deletion)
 
 

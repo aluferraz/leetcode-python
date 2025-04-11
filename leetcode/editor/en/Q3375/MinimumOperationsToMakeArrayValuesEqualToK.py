70716f70a1d8eddce1cf582059672864febdb75b
Lucas Mohallem Ferraz <ferraz.m.lucas@gmail.com>
2025-04-11 03:29:03 -0700

    110425

--- leetcode/editor/en/Q3375/MinimumOperationsToMakeArrayValuesEqualToK.py
+++ leetcode/editor/en/Q3375/MinimumOperationsToMakeArrayValuesEqualToK.py
@@ -1,10 +1,17 @@
-from typing import List, Optional
-from functools import cache
+from typing import List
+
 
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
-        
+        nlist = list(sorted(set(nums)))
+        if nlist[0] < k:
+            return -1
+        if nlist[0] == k:
+            return len(nlist) - 1
+        return len(nlist)
+
+
 # leetcode submit region end(Prohibit modification and deletion)
 
 
